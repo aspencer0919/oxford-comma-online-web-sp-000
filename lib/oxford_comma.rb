25fa2322daf115e257614aa332833e2dbc498c38
Adam Spencer <aspencer0919@gmail.com>
2020-04-01 19:54:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,14 +3,16 @@ def oxford_comma(array)
     return "#{array[0]}"
   elsif array.size == 2
     return "#{array[0]} and #{array[1]}"
-  else
-    #counter == 0
-    while words < array.size
-      array.each do |x|
-        return "#{x}, "
-        words += 1
-      end
-    return last(array)
-    end
+  elsif array.length > 2
+    array[-1].prepend "and "
+  array.join(", ")
+#    #counter == 0
+#    while words < array.size
+#      array.each do |x|
+#        return "#{x}, "
+#        words += 1
+#      end
+#    return last(array)
+#    end
   end
 end
